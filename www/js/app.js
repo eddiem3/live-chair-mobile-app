836b48320fdfe34ae74d52858af0c9e72e3d8906
Eddie Massey III <eem33@case.edu>
2014-11-04 15:02:47 -0500

    display reviews

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,16 @@
 (function(){
     'use strict';
     var module = angular.module('app', ['onsen']);
-
+    
     module.controller('AppController', function($scope, $data) {
-      $scope.doSomething = function() {
-	setTimeout(function() {
-          alert('tappaed');
-      }, 100);
-    };
+	$scope.doSomething = function() {
+	    setTimeout(function() {
+		alert('tappaed');
+	    }, 100);
+	};
+	
+	
+
 	
 	$scope.getTimes=function(n){
 	    return new Array(parseInt(n));
@@ -36,34 +39,92 @@
           { 
               fname: 'John',
               lname: 'Doe',
+	      avatar: '../img/barber1.jpg',
               shop_name: 'Doe Cutz',
 	      address: '123 Street Ave.',
 	      city: 'Orangeburg',
 	      state:'SC',
 	      zip: '29115',
-	      rating: '3'
+	      rating: '3',
+
+	      reviews: [{
+		  stars: 1,
+		  body: "This gem is WAY too expensive for its rarity value.",
+		  author: "turtleguyy@example.org",
+		  createdOn: 1397490980837
+	      },
+		 {
+		     stars: 1,
+		     body: "BBW: High Shine != High Quality.",
+		     author: "LouisW407@example.org",
+		     createdOn: 1397490980837
+		 }, {
+		     stars: 1,
+		     body: "Don't waste your rubles!",
+		     author: "nat@example.org",
+		     createdOn: 1397490980837
+		 }]	      
           },
 
           { 
 	      fname: 'John',
               lname: 'Doe',
+	      avatar: '../img/barber2.jpg',
               shop_name: 'Family Barber',
 	      address: '123 Street Ave.',
 	      city: 'Seattle',
 	      state:'WA',
 	      zip: '98052',
-	      rating: '5'
+	      rating: '5',
+
+	        reviews: [{
+		  stars: 1,
+		  body: "Buddy can't cut bo.",
+		  author: "turtleguyy@example.org",
+		  createdOn: 1397490980837
+	      },
+		 {
+		     stars: 5,
+		     body: "Real talk. He da truth.",
+		     author: "LouisW407@example.org",
+		     createdOn: 1397490980837
+		 }, {
+		     stars: 2,
+		     body: "Don't waste your money bruh!",
+		     author: "nat@example.org",
+		     createdOn: 1397490980837
+		 }]	      
+
           },
 
           { 
 	      fname: 'John',
               lname: 'Doe',
+	      avatar: '../img/barber3.jpg',
               shop_name: 'True',
 	      address: '123 Street Ave.',
 	      city: 'Cleveland',
 	      state:'OH',
 	      zip: '44106',
-	      rating: '2'
+	      rating: '2',
+
+	        reviews: [{
+		  stars: 1,
+		  body: "This gem is WAY too expensive for its rarity value.",
+		  author: "turtleguyy@example.org",
+		  createdOn: 1397490980837
+	      },
+		 {
+		     stars: 1,
+		     body: "BBW: High Shine != High Quality.",
+		     author: "LouisW407@example.org",
+		     createdOn: 1397490980837
+		 }, {
+		     stars: 1,
+		     body: "Don't waste your rubles!",
+		     author: "nat@example.org",
+		     createdOn: 1397490980837
+		 }]	      
           }
       ]; 
       
